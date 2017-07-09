371bc6cbb6926072e7ec3283e142f37ebaac2a8a
Sidtharthan_A_N <sidthunagarajan@gmail.com>
2017-07-09 14:41:41 +0530

    tests restructured. test-suite/exec-v2 introduced to run test with custom description

--- src/projecteulerclj/test_suite.clj
+++ src/projecteulerclj/test_suite.clj
@@ -2,6 +2,12 @@
 
 (defn log [& args] (println (apply str args)))
 
+(defn describe [name desc]
+  (log "=======================================================")
+  (log "Problem :" name)
+  (log "Desc :" desc)
+  (log "======================================================="))
+
 (defn exec-one [main input expected]
   (log "INPUT :" input)
   (log "EXPECTED :" expected)
@@ -12,21 +18,34 @@
           (log "ACTUAL :" actual))))
   (log "________________________________"))
 
+(defn exec-one-v2 [scene main input expected]
+  (log "SCENE :" scene)
+  (let [actual (apply main input)]
+    (if (= expected actual)
+      (log "STATUS : OK")
+      (do (log "EXPECTED :" expected)
+          (log "ACTUAL :" actual)
+          (log "STATUS :" "FAIL" "*******************************"))))
+  (log "________________________________"))
+
 (defn exec [name desc main & cases]
-  (log "=======================================================")
-  (log "Problem :" name)
-  (log "Desc :" desc)
-  (log "=======================================================")
+  (describe name desc)
   (doseq [case cases
           :let [[[& input] expected] case]]
     (exec-one main input expected)))
 
 (defn exec1.1 [name desc main & ins-outs]
-  (log "=======================================================")
-  (log "Problem :" name)
-  (log "Desc :" desc)
-  (log "=======================================================")
+  (describe name desc)
   (loop [rest-ins-outs ins-outs]
     (when rest-ins-outs
       (exec-one main (first rest-ins-outs) (second rest-ins-outs))
       (recur (nnext rest-ins-outs)))))
+
+(defn exec-v2 [name desc main & in-outs]
+  (describe name desc)
+  (doseq [{input    :in
+           expected :out
+           scene    :scene} in-outs]
+    (if scene
+      (exec-one-v2 scene main input expected)
+      (exec-one main input expected))))
