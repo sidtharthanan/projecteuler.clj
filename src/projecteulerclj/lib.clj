027156eaa3fc7ef533454ef30f29541efa5d8621
Sidtharthan_A_N <sidthunagarajan@gmail.com>
2017-10-31 13:50:52 +0530

    Add divisible-by? to lib. Refactor problem8 and problem3

--- src/projecteulerclj/lib.clj
+++ src/projecteulerclj/lib.clj
@@ -8,12 +8,15 @@
 
 (def rem? #(not (zero? (rem %1 %2))))
 
+(defn divisible-by? [dividend divisor]
+  (zero? (rem dividend divisor)))
+
 (defn take-below [max-xcld range]
   (take-while #(< % max-xcld) range))
 
 (defn gcd [a b]
   "greatest common divisor"
-  (if-not (rem? a b) b (gcd b (rem a b))))
+  (if (divisible-by? a b) b (gcd b (rem a b))))
 
 (defn lcm [& args]
   "least common multiple"
@@ -31,7 +34,7 @@
 (defn lpf [number]
   "least prime factor"
   (if (> number 2)
-    (some (fn [factor] (if-not (rem? number factor) factor))
+    (some (fn [factor] (if (divisible-by? number factor) factor))
           (take-below (prime-factor-limit number) (prime-candidates)))))
 
 (defn prime? [number] (not (lpf number)))
@@ -43,6 +46,8 @@
 (defn frame [size coll]
   (if (<= size (count coll))
     (cons (take size coll) (lazy-seq (frame size (rest coll))))))
+
+(def frame #(partition %1 1 %2))
 
 (defn pythagorean? [a b c]
   (and (< a b c) (= (+ (* a a) (* b b)) (* c c))))
--- src/projecteulerclj/problem3.clj
+++ src/projecteulerclj/problem3.clj
@@ -9,8 +9,26 @@
      (factorize (/ number factor) (conj factors factor))
      (conj factors number))))
 
+(defn factorize-1 [product]
+  (loop [dividend product factors []]
+    (if-let [factor (lpf dividend)]
+      (recur (/ dividend factor) (conj factors factor))
+      (conj factors dividend))))
+
+(defn factorize-2 [number]
+  (fn [factors]
+    (if-let [factor (lpf number)]
+      ((factorize-2 (/ number factor)) (conj factors factor))
+      (conj factors number))))
+
 (defn main [number]
   (apply max (factorize number)))
+
+(defn main-1 [number]
+  (apply max (factorize-1 number)))
+
+(defn main-2 [number]
+  (apply max ((factorize-2 number) (set nil))))
 
 (defn problem3 []
   (tests/exec "problem3" "INPUT -> factos -> primes -> max ->" main
