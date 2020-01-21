6074f29106b63aa473222c644e5c36a6ae19c2ec
sidtharthan <sidthunagarajan@gmail.com>
2020-01-21 11:40:59 +0530

    collatz tree with simple vector instead of clojure record

--- src/projecteulerclj/problem14.clj
+++ src/projecteulerclj/problem14.clj
@@ -1,45 +1,37 @@
 (ns projecteulerclj.problem14
   (:require [projecteulerclj.test-suite :refer :all]))
 
-(defrecord Tree [node left right])
-
-(defn tovec [t]
-  [(:node t)
-   (and (:left t) (tovec (:left t)))
-   (and (:right t) (tovec (:right t)))])
-
-(defn totree [[node left right :as t]]
-  (if (not (nil? t)) (Tree. node (totree left) (totree right))))
-
-(defn merge-trees [t1 t2]
+(defn merge-trees [[n1 l1 r1 :as t1] [n2 l2 r2 :as t2]]
   (cond
     (nil? t1) t2
     (nil? t2) t1
-    :else     (Tree. (:node t1) (merge-trees (:left t1) (:left t2)) (merge-trees (:right t1) (:right t2)))))
+    :else     [n1
+               (merge-trees l1 l2)
+               (merge-trees r1 r2)]))
 
-(defn insert-branch [tree branch [side & path]]
-  (if (nil? side)
+(defn insert-branch [tree branch [index & path]]
+  (if (nil? index)
     (merge-trees tree branch)
-    (assoc tree side (insert-branch (get tree side) branch path))))
+    (assoc tree index (insert-branch (tree index) branch path))))
 
-(defn common-path [tree node]
-  (loop [[tree path continue] [tree [] nil]]
+(defn common-path [tree n]
+  (loop [[[node left right :as tree] path continue] [tree [] nil]]
     (cond
-      (nil? tree)                (if (nil? continue) nil (recur continue))
-      (= (:node tree) node)      path
-      :else                      (recur
-                                   [(:left tree)
-                                    (conj path :left)
-                                    [(:right tree) (conj path :right) continue]]))))
+      (nil? tree) (if (nil? continue) nil (recur continue))
+      (= node n)  path
+      :else       (recur
+                    [left
+                     (conj path 1)
+                     [right (conj path 2) continue]]))))
 
-(defn collatz-next [tree]
-  (if (even? (:node tree))
-    (Tree. (/ (:node tree) 2) tree nil)
-    (Tree. (inc (* (:node tree) 3)) nil tree)))
+(defn collatz-next [[node :as tree]]
+  (if (even? node)
+    [(/ node 2) tree nil]
+    [(inc (* node 3)) nil tree]))
 
 (defn builder [tree n]
-  (loop [branch (Tree. n nil nil)]
-    (let [path (common-path tree (:node branch))]
+  (loop [[node :as branch] [n]]
+    (let [path (common-path tree node)]
       (if
         (nil? path)
         (recur (collatz-next branch))
@@ -48,19 +40,20 @@
 (defn collatz-tree [limit]
   (reduce
    builder
-   (Tree. 1 nil nil)
+   [1]
    (range 3 limit)))
 
 (defn main [n]
   (println "hi")
-  (println (tovec (collatz-tree n))))
+  (println (collatz-tree n)))
 
 (defn problem14 []
   (exec-v2 "problem14"
            "Collatz sequence: Which starting number, under <NUMBER n>, produces the longest chain?"
            main
            {:scene "test" :in [10000] :out 1000}
-;           {:scene "Below 8 is 7" :in [8] :out 7}
-;           {:scene "Below 10 is 9" :in [10] :out 9}
-;           {:scene "Below 15 is 9" :in [15] :out 9}
+
+           ;           {:scene "Below 8 is 7" :in [8] :out 7}
+           ;           {:scene "Below 10 is 9" :in [10] :out 9}
+           ;           {:scene "Below 15 is 9" :in [15] :out 9}
            ))
