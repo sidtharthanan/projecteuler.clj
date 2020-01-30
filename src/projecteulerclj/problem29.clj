4e828a1f7f1479ab8b39de147dd083cad5e82367
sidtharthan <sidthunagarajan@gmail.com>
2020-01-30 21:36:13 +0530

    29 solved

---
+++ src/projecteulerclj/problem29.clj
@@ -0,0 +1,16 @@
+(ns projecteulerclj.problem29
+  (:require [projecteulerclj.test-suite :refer :all]))
+
+(defn main [limit]
+  (count
+   (reduce conj #{}
+           (for [x (range 2 (inc limit))
+                 y (range 2 (inc limit))]
+             (.pow (biginteger x) y)))))
+
+(defn problem29 []
+  (exec-v2 "problem29"
+           ""
+           main
+           {:in [5] :out 15}
+           {:in [100] :out 9183}))
